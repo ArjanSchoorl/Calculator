fbe58dd5b9f30f79175f2795a491b2076cf876b8
Arjan Schoorl <schoorl.arjan@outlook.com>
2019-02-27 09:07:08 +0100

    Removed ask function to continue

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,16 @@
 from time import sleep
 import sys
 
-# Give number
-# Give operator
-# Give number 2
-# Check numbers and operator
-# Print the result
-# Restart
-
-def check_num(val):
+# This function checks if the val is a float or not
+def check_number(val):
     try:
         float(val)
         return True
     except ValueError:
         return False
 
-
-def check_op(val):
+# This function checks what operator val is
+def check_operator(val):
     if val == "-":
         return True
     elif val == "+":
@@ -27,83 +21,44 @@ def check_op(val):
         return True
     return False
 
-def user_restart():
-    answer = input("Continue? Yes or No: ").lower()
-    if answer == "yes" or answer == "y":
-        return True
-    return False
-
-
-i = 0
+# Declare state and print calculator
+state = 0
 print("Calculator")
 
+# Start the calculator
 while True:
-    while i == 0:
+    while state == 0:
         print("")
-        number = input("Number one: ")
+        number_1 = input("Number one: ")
         operator = str(input("Operator (+-*/): "))
-        number2 = input("Number two: ")
-        i = 1
+        number_2 = input("Number two: ")
+        state = 1
 
-    while i == 1:
-        if not check_num(number):
+    while state == 1:
+        if not check_number(number_1):
             print("Number 'one' is not a number")
             sleep(1)
-            i = 0
-        elif not check_num(number2):
+            state = 0
+        elif not check_number(number_2):
             print("Number 'two' is not a number")
             sleep(1)
-            i = 0
-        elif not check_op(operator):
+            state = 0
+        elif not check_operator(operator):
             print("You did not use a operator")
             sleep(1)
-            i = 0
+            state = 0
         else:
             if operator == "-":
-                num = float(number)
-                num2 = float(number2)
-                result = num - num2
-                print("Answer: " + str(result))
-                if user_restart():
-                    i = 0
-                else:
-                    print("Thank you for using this calculator!")
-                    sleep(2)
-                    sys.exit()
+                print("Answer: " + str(float(number_1) - float(number_2)))
+                state = 0
             elif operator == "+":
-                num = float(number)
-                num2 = float(number2)
-                result = num + num2
-                print("Answer: " + str(result))
-                if user_restart():
-                    i = 0
-                else:
-                    print("Thank you for using this calculator!")
-                    sleep(2)
-                    sys.exit()
+                print("Answer: " + str(float(number_1) + float(number_2)))
+                state = 0
             elif operator == "*":
-                num = float(number)
-                num2 = float(number2)
-                result = num * num2
-                print("Answer: " + str(result))
-                if user_restart():
-                    i = 0
-                else:
-                    print("Thank you for using this calculator!")
-                    sleep(2)
-                    sys.exit()
+                print("Answer: " + str(float(number_1) * float(number_2)))
+                state = 0
             elif operator == "/":
-                num = float(number)
-                num2 = float(number2)
-                result = num / num2
-                print("Answer: " + str(result))
-                if user_restart():
-                    i = 0
-                else:
-                    print("Thank you for using this calculator!")
-                    sleep(2)
-                    sys.exit()
+                print("Answer: " + str(float(number_1) / float(number_2)))
+                state = 0
             else:
-                print("Thank you for using this calculator!")
-                sleep(2)
-                sys.exit()
+                print("There went something wrong!")
